ce69738b530566f4e21bf6c46b59abbc88deece3
anhhuynh <anhhuy@metropolia.fi>
2025-06-16 09:05:06 +0300

    Document assignment

--- src/SecondClass.java
+++ src/SecondClass.java
@@ -1,8 +1,27 @@
-//print 2 lines at the same time
 public class SecondClass {
 
     public static void main(String[] args) {
+        //print 2 lines at the same time
         System.out.println("Hello World");
-        System.out.print("Hello Anh");
+        System.out.println("Hello Anh");
+
+        //conditional statement if-then
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien");
+            System.out.println("And I am scared of aliens");
+        }
+
+        //advanced conditional statement
+        int topScore = 80;
+        if (topScore <= 100) {
+            System.out.println("You got the high score");
+        }
+
+        int secondTopScore = 60;
+        if (topScore > secondTopsScore && topScore <100) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+
     }
 }
